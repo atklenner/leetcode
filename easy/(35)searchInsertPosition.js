6d86877c5875a798f6c1104e480eb54d2ff010c3
Andrew Klenner <atklenner@gmail.com>
2022-10-27 20:45:53 -0700

    finished

---
+++ easy/(35)searchInsertPosition.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+  // write a binary search
+  let min = 0;
+  let max = nums.length - 1;
+  while (min <= max) {
+    let middle = Math.floor((min + max) / 2);
+    let guess = nums[middle];
+    // if the value is found, return the index
+    if (guess === target) return middle;
+    if (guess < target) min = middle + 1;
+    if (guess > target) max = middle - 1;
+  }
+  // if the value is not found, return the min index
+  return min;
+};
